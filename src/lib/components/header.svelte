6c19c52f9751df616f13616c7d62061d103c42eb
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-09 21:23:10 -0300

    config: style shadcn and tailwind

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ThemeToggle from './theme-toggle.svelte';
+</script>
+
+<header class="w-full max-w-full flex items-center justify-between p-6">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Tickets</h1>
+	<ThemeToggle />
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from '$lib/components/header.svelte';
+	import '../app.postcss';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<ModeWatcher />
+<Header />
+<main class="w-full max-w-4xl mx-auto">
+	<slot />
+</main>
