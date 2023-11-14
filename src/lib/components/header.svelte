7418f6be542d9630c71b376aa5e8a2bf5e4910b7
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-14 15:56:46 -0300

    feat: signout button in header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+  import { LogOut } from 'lucide-svelte'
   import ThemeToggle from './theme-toggle.svelte'
+  import Button from './ui/button/button.svelte'
+  import type { Session } from 'lucia'
+
+  export let session: Session | null
 </script>
 
-<header class="w-full max-w-full flex items-center justify-between p-6">
+<header
+  class="w-full max-w-full flex items-center justify-between border-b p-6"
+>
   <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
     Tickets
   </h1>
-  <ThemeToggle />
+  <div class="flex items-center gap-4">
+    <ThemeToggle />
+    {#if session}
+      <form method="POST" action="/?/logout">
+        <Button type="submit"><LogOut class="mr-2 w-4 h-4" />Sair</Button>
+      </form>
+    {/if}
+  </div>
 </header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
   import Header from '$lib/components/header.svelte'
   import '../app.postcss'
   import { ModeWatcher } from 'mode-watcher'
+
+  export let data
 </script>
 
 <ModeWatcher />
-<Header />
+<Header session={data.session} />
 <main class="w-full max-w-4xl mx-auto">
   <slot />
 </main>
