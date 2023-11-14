2f26e27d09b811e3ca7bd38132f688839542f374
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-14 19:02:49 -0300

    feat: view transitions

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,23 +1,52 @@
 <script lang="ts">
-  import { LogOut } from 'lucide-svelte'
+  import { Loader2, LogOut } from 'lucide-svelte'
   import ThemeToggle from './theme-toggle.svelte'
   import Button from './ui/button/button.svelte'
   import type { Session } from 'lucia'
+  import { buttonVariants } from './ui/button'
+  import { enhance } from '$app/forms'
+  import { goto } from '$app/navigation'
 
   export let session: Session | null
+
+  let isLoading = false
 </script>
 
 <header
   class="w-full max-w-full flex items-center justify-between border-b p-6"
 >
-  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Tickets
-  </h1>
+  <a href="/">
+    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Tickets
+    </h1>
+  </a>
   <div class="flex items-center gap-4">
     <ThemeToggle />
     {#if session}
-      <form method="POST" action="/?/logout">
-        <Button type="submit"><LogOut class="mr-2 w-4 h-4" />Sair</Button>
+      {#if session.user.role === 'admin'}
+        <a class={buttonVariants()} href="/admin/new-user">Admin</a>
+      {/if}
+      <form
+        method="POST"
+        action="/?/logout"
+        use:enhance={({}) => {
+          isLoading = true
+
+          return ({ result }) => {
+            if (result) {
+              isLoading = false
+              session = null
+              goto('/auth/sign-in')
+            }
+          }
+        }}
+      >
+        <Button disabled={isLoading} type="submit">
+          {#if isLoading}
+            <Loader2 class="mr-2 w-4 h-4" />
+          {:else}
+            <LogOut class="mr-2 w-4 h-4" />{/if}Sair</Button
+        >
       </form>
     {/if}
   </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,25 @@
-<script>
+<script lang="ts">
   import Header from '$lib/components/header.svelte'
   import '../app.postcss'
   import { ModeWatcher } from 'mode-watcher'
+  import { onNavigate } from '$app/navigation'
 
   export let data
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve()
+        await navigation.complete
+      })
+    })
+  })
 </script>
 
 <ModeWatcher />
 <Header session={data.session} />
-<main class="w-full max-w-4xl mx-auto">
+<main class="w-full max-w-4xl mx-auto py-6">
   <slot />
 </main>
